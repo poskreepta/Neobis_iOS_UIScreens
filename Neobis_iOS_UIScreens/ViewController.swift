ed0841d476c24476916e524811b3f579cd80c962
poskreepta <poskreepta@yandex.ru>
2023-05-08 23:45:55 +0400

    Initial Commit

---
+++ Neobis_iOS_UIScreens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_UIScreens
+//
+//  Created by poskreepta on 08.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
